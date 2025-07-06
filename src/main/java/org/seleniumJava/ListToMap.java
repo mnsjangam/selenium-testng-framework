baadf0d3fa89c48c80fe0a5bfa68a58b201506c2
Mansi Jangam <mnsjangam@gmail.com>
2025-07-05 23:06:43 -0500

    more practise code - Collection operations

---
+++ src/main/java/org/seleniumJava/ListToMap.java
@@ -0,0 +1,44 @@
+package org.seleniumJava;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ListToMap {
+
+    public static void main(String[] args) {
+        // Example usage of the ListToMap class
+        System.out.println("List to Map example");
+
+        Student student1 = new Student("Alice", "Broadway");
+        Student student2 = new Student("Bob", "Lakeview");
+        Student student3 = new Student("Charlie", "Charlotte");
+
+        List<Student> list = new ArrayList<Student>();
+
+        Map<Student, Integer> map = new HashMap<Student, Integer>();
+        map.put(student1, 1);
+        map.put(student2, 2);
+        map.put(student3, 3);
+
+        for (Map.Entry<Student, Integer> entry : map.entrySet()){
+            System.out.println("Key: " + entry.getKey() +" "+ entry.getValue());
+        }
+    }
+
+    public static class Student {
+        String name;
+        String lastName;
+
+        public Student(String name, String lastName){
+            this.lastName=lastName;
+            this.name=name;
+        }
+
+        @Override
+        public String toString() {
+            return "Student [lastName=" + lastName + ", name=" + name + "]";
+        }
+    }
+}
---
+++ src/main/java/org/seleniumJava/MorePractise.java
@@ -0,0 +1,37 @@
+package org.seleniumJava;
+
+import com.google.common.base.Strings;
+
+import java.util.*;
+
+public class MorePractise {
+    public Map<Integer, Integer> map = new HashMap<>();
+    public static void main(String[] args) {
+        MorePractise morePractise = new MorePractise();
+        // int n = morePractise.fibonacciSeries(5);
+
+        morePractise.stringToCharList("Java Welcome to Selenium");
+    }
+
+    public int fibonacciSeries(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+        int result = fibonacciSeries(n - 1) + fibonacciSeries(n - 2);
+        map.put(n, result);
+        System.out.print(result + " ");
+        return result;
+    }
+
+    public void stringToCharList(String str) {
+        List<Character> charList = new ArrayList<>();
+        for (int i=0; i< str.length(); i++){
+            charList.add(str.charAt(i));
+        }
+        System.out.println("Character List: " + charList);
+    }
+}
